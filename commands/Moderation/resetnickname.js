32013735a477018410dac1352484f4e33cec6306
xavierk09 <xaviervavierk@gmail.com>
2021-04-20 17:16:03 +0530

    8#

--- commands/Moderation/resetnickname.js
+++ commands/Moderation/resetnickname.js
@@ -3,6 +3,10 @@ module.exports = {
     cooldown: 10,
     aliases: ['resetnick'],
     async execute(client, command, message, args, Discord) {
+
+        if(!message.member.hasPermission("MANAGE_NICKNAMES")) return message.channel.send("> You need the `MANAGE_NICKNAMES` permission!")
+        if(!message.guild.me.hasPermission("MANAGE_NICKNAMES")) return message.channel.send("> I do not have the `MANAGE_NICKNAMES` permission!")
+
         const member = message.mentions.members.first() || message.member;
 
         if (!member) return message.channel.send(`> Please specify a member, ${message.author.username}!`);
