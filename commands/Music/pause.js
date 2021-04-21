e20e95bcc47117947cc1ce69a4606a3208e28e13
xavierk09 <xaviervavierk@gmail.com>
2021-04-21 12:36:43 +0530

    14#

---
+++ commands/Music/pause.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'pause',
+    async execute(client, command, message, args, Discord){
+        if (!message.member.voice.channel) return message.channel.send('> You must be in a voice channel to execute this command!')
+
+        client.distube.pause(message);
+
+        message.react('⏯');
+
+    }
+}
---
+++ commands/Music/resume.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'resume',
+    async execute(client, command, message, args, Discord){
+        if (!message.member.voice.channel) return message.channel.send('> You must be in a voice channel to execute this command!')
+
+        client.distube.resume(message);
+
+        message.react('⏯');
+
+    }
+}
