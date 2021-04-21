64c199991a2f1da6a85e3fbeced11a2aca0bfaa1
xavierk09 <xaviervavierk@gmail.com>
2021-04-21 13:49:19 +0530

    23#

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -1,20 +1,22 @@
+const { MessageEmbed } = require('discord.js');
 module.exports = {
   name: '8ball',
   async execute(client, command, message, args, Discord) {
-    if (!args[0]) return message.channel.send('Please ask a full question!'); 
-    const replies = ['HELL YEA!', 'HELL NO!', 'NEVERRR!', 'DEFINITELYYY!!', 'sadly '];
+    if (!args[0]) return message.channel.send('Please ask a full question!'); // return if no question is commenced
+    const replies = ['Yes.', 'No.', 'Never.', 'Definitely.', 'Ask again later.']; // random responses
 
-    const result = Math.floor(Math.random() * replies.length); 
-    const question = args.join(' '); 
+    const result = Math.floor(Math.random() * replies.length); // Get a random respons for the array
+    const question = args.join(' '); // join the args(Array<string>) to a question string
+    // check permissions for embed
     if (message.channel.permissionsFor(message.guild.me).has('EMBED_LINKS')) {
-      const replies = new Discord.MessageEmbed()
+      const embed = new MessageEmbed() // create embed 
         .setAuthor('🎱 The 8 Ball says...')
         .setColor('ORANGE').addField('Question:', question)
         .addField('Answer:', replies[result])
         .setFooter(`Asked by ${message.author.username}`);
-      await message.channel.send(replies);
+      await message.channel.send(embed); // send embed message
     } else {
-      await message.channel.send(`**Question:**\n${question}\n**Answer:**\n${replies[result]}`);
+      await message.channel.send(`**Question:**\n${question}\n**Answer:**\n${replies[result]}`); // no permissins so bot will default to a raw message
     }
   },
 };
