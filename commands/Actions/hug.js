9e6217719a3102ec019026bfc2b496a38b8e8ac5
xavierk09 <xaviervavierk@gmail.com>
2021-04-21 15:00:31 +0530

    25#

--- commands/Actions/hug.js
+++ commands/Actions/hug.js
@@ -8,6 +8,12 @@ module.exports = {
             "https://media.tenor.com/images/aab83bd3725feeaccb9929f8ca964db9/tenor.gif",
             "https://64.media.tumblr.com/tumblr_ma7l17EWnk1rq65rlo1_500.gif",
             "https://i.pinimg.com/originals/63/63/06/636306b78e9a992418f10c427a0a3b66.gif",
+            "https://media.tenor.com/images/accfef6ee4cab73f9b2caf2e34ec4380/tenor.gif",
+            "https://pa1.narvii.com/6110/b4d5e1e0c775f73b48a4a76afb3917b574a71c35_hq.gif",
+            "https://data.whicdn.com/images/213476418/original.gif",
+            "https://64.media.tumblr.com/e149a5fbabdfd6531f29bba15703bdf5/tumblr_mlzqwoeHzE1sn5lrwo1_500.gif",
+            "https://i.pinimg.com/originals/e6/36/47/e636479dafb6be4d4a6c23e2c52a42bf.gif",
+            "https://media1.tenor.com/images/50c0ec78664fe643ce2ba898232b2282/tenor.gif",
         ];
         let pick = gifs[Math.floor(Math.random() * gifs.length)];
 
