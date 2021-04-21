ce8e51efbed68ab97d7b8c2928fea75761df9ee3
xavierk09 <xaviervavierk@gmail.com>
2021-04-21 12:08:55 +0530

    13#

--- commands/Utility/help.js
+++ commands/Utility/help.js
@@ -10,7 +10,7 @@ module.exports = {
             .setThumbnail('https://cdn.discordapp.com/attachments/822106850258321440/822432415364546580/nearrrfulll.jpg')
             .addFields(
                 { name: '🎵 Music', value: '`play`, `stop`, `skip`, `queue`\n`loop`' },
-                { name: '😂 Fun', value: '`dankmeme`, `wholesomememe`' },
+                { name: '😂 Fun', value: '`dankmeme`, `wholesomememe`, `8ball`' },
                 { name: '🔧 Utility', value: '`avatar`, `mcserver`, `ping`\n`corona`, `reportbug`,`anime`, `embed`\n`updatelogs`' },
                 { name: '⚙  Moderation', value: '`ban`, `unban`, `kick`, `clear`\n`nickname`, `resetnickname`' }
             )
