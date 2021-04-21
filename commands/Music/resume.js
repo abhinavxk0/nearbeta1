64cbf0721568537fe5793817d43f54e89cd504aa
xavierk09 <xaviervavierk@gmail.com>
2021-04-21 14:24:55 +0530

    26#

--- commands/Music/resume.js
+++ commands/Music/resume.js
@@ -1,5 +1,6 @@
 module.exports = {
     name: 'resume',
+    aliases: ['unpause'],
     async execute(client, command, message, args, Discord){
         if (!message.member.voice.channel) return message.channel.send('> You must be in a voice channel to execute this command!')
 
