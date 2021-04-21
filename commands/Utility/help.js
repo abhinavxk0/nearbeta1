59f2d943632427770d438c3e6594fae9d9281681
xavierk09 <xaviervavierk@gmail.com>
2021-04-21 14:21:53 +0530

    25#

--- commands/Utility/help.js
+++ commands/Utility/help.js
@@ -9,9 +9,10 @@ module.exports = {
             .setTitle('Help Command')
             .setThumbnail('https://cdn.discordapp.com/attachments/822106850258321440/822432415364546580/nearrrfulll.jpg')
             .addFields(
+                { name: '🔧 Utility', value: '`avatar`, `mcserver`, `ping`\n`corona`, `reportbug`,`anime`, `embed`\n`updatelogs`' },
                 { name: '🎵 Music', value: '`play`, `stop`, `skip`, `queue`\n`loop`, `resume`, `pause`, `volume`' },
                 { name: '😂 Fun', value: '`dankmeme`, `wholesomememe`, `8ball`' },
-                { name: '🔧 Utility', value: '`avatar`, `mcserver`, `ping`\n`corona`, `reportbug`,`anime`, `embed`\n`updatelogs`' },
+                { name: '🙋‍♂️ Actions', value: '`hug`'},
                 { name: '⚙  Moderation', value: '`ban`, `unban`, `kick`, `clear`\n`nickname`, `resetnickname`' }
             )
             .setFooter(`${message.author.username} have a amazing day ahead!`);
@@ -19,3 +20,5 @@ module.exports = {
     }
 }
 
+
+                
