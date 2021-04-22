1c945d2afbe202bd79b4cf62a368ff6d0c007acf
xavierk09 <xaviervavierk@gmail.com>
2021-04-22 15:59:38 +0530

    31#

--- commands/Music/volume.js
+++ commands/Music/volume.js
@@ -5,7 +5,7 @@ module.exports = {
     aliases: ['vol'],
     async execute(client, command, message, args, Discord){
         if (!message.member.voice.channel) return message.channel.send('> You must be in a voice channel to execute this command!')
-        if (args[0] > 100) return message.channel.send("> Who the he*k are you trying to ear-ra*e?")
+        if (args[0] > 100) return message.channel.send("> Who the heck are you trying to ear-ra*e?")
         
         client.distube.setVolume(message, args[0]);
         message.channel.send(`> Volume set to ${args[0]}`)
