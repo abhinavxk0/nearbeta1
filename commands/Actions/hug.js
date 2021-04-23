9571efc56e5a5bf82ff9b80c58f34f51709b98da
xavierk09 <xaviervavierk@gmail.com>
2021-04-23 10:31:06 +0530

    41#

--- commands/Actions/hug.js
+++ commands/Actions/hug.js
@@ -31,8 +31,10 @@ module.exports = {
 
         if (args[0]) {
             let user = message.mentions.members.first();
+            embed.setTitle(':)')
             embed.setDescription(`${message.author.username} hugs ${args[0]}!`)
         } else {
+            embed.setTitle(':)')
             embed.setDescription(`${message.author.username} wants a hug!`)
         }
         message.channel.send(embed);
