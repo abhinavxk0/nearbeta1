a4a69eb696d845d51cc71ded2072529276b4d3e2
xavierk09 <xaviervavierk@gmail.com>
2021-04-30 12:14:26 +0530

    59#
    concised

--- index.js
+++ index.js
@@ -6,38 +6,22 @@ const db = require('quick.db');
 const got = require('got');
 const DisTube = require('distube');
 const prefix = '-';
-//const npEmbed = new Discord.MessageEmbed()
-// .setColor('RANDOM')
-// .setTitle(`Now Playing: ${song.name}-${song.formattedDuration}`)
-// .setFooter(`Requested by: ${song.user}`)
-
-// const addsEmbed = new Discord.MessageEmbed()
-// .setColor('RANDOM')
-// .setTitle(`Added: ${song.name}-${song.formattedDuration}`)
-// .setFooter(`Added by: ${song.user}`)
-
-// const emptyEmbed = new Discord.MessageEmbed()
-// .setColor('RANDOM')
-// .setTitle("Voice chanel is empty, stopping playback and leaving channel.")
 client.cooldowns = new Discord.Collection();
-
 client.distube = new DisTube(client, { searchSongs: false, emitNewSongOnly: true, leaveOnEmpty: true, });
-
 client.distube.on("playSong", (message, queue, song) => message.channel.send(
     new Discord.MessageEmbed()
         .setColor('#b0e0e6')
         .setTitle(`Now Playing: ${song.name}-${song.formattedDuration}`)
         .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
         .setDescription(`Requested by: ${song.user}`)
-    
-    ))
+))
 client.distube.on("addSong", (message, queue, song) => message.channel.send(
     new Discord.MessageEmbed()
         .setColor('#e6b0e0')
         .setTitle(`Added: ${song.name}-${song.formattedDuration}`)
         .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
         .setDescription(`Added by: ${song.user}`)
-    ))
+))
 client.distube.on("empty", message => message.channel.send(
     new Discord.MessageEmbed()
         .setColor('#e0e6b0')
@@ -45,10 +29,8 @@ client.distube.on("empty", message => message.channel.send(
         .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
         .setDescription('Reason: Disconnect because voice channel empty!')
 ))
-
 client.commands = new Discord.Collection();
 const commandFolders = fs.readdirSync('./commands');
-
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for (const folder of commandFolders) {
     const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
@@ -60,45 +42,33 @@ for (const folder of commandFolders) {
 client.once('ready', () => {
     console.log('NearBeta is online.')
     client.user.setPresence({
-         activity: { 
+        activity: {
             name: '-help || -ping',
             type: "LISTENING"
-        }, 
-        status: 'online' })
+        },
+        status: 'online'
+    })
         .catch(console.error);
     // client.user.setActivity(db.get(`status`))
 })
-
 client.on('message', async message => {
     if (!message.content.startsWith(prefix) || message.author.bot || (!message.guild)) return;
-    if(message.mentions.members.first()){
-        if(db.has(`afk-${message.mentions.members.first().id}+${message.guild.id}`)) {
-            
-        }
-    }
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
-    //commmand sorting and executing bullshit
-    const command = client.commands.get(commandName)
-        || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
     if (!command) return;
-
     if (command.args && !args.length) {
         return message.channel.send(`> You didn't provide any arguments, ${message.author}!`);
     }
     const { cooldowns } = client;
-
     if (!cooldowns.has(command.name)) {
         cooldowns.set(command.name, new Discord.Collection());
     }
-
     const now = Date.now();
     const timestamps = cooldowns.get(command.name);
     const cooldownAmount = (command.cooldown || 3) * 1000;
-
     if (timestamps.has(message.author.id)) {
         const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
         if (now < expirationTime) {
             const timeLeft = (expirationTime - now) / 1000;
             return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
@@ -106,7 +76,6 @@ client.on('message', async message => {
     }
     timestamps.set(message.author.id, now);
     setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
     try {
         command.execute(client, command, message, args, Discord);
     } catch (error) {
