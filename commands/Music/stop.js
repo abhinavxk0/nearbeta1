ed0865a0d5a174616e7e86c30e2cbcaaa6e5c301
xavierk09 <xaviervavierk@gmail.com>
2021-04-30 07:26:08 +0530

    55#
    stop, dj perm

--- commands/Music/stop.js
+++ commands/Music/stop.js
@@ -7,6 +7,9 @@ module.exports = {
     description: 'stops music',
     async execute(client, command, message, args, Discord) {
         if (!message.member.voice.channel) return message.channel.send('> You must be in a voice channel to execute this command!')
+        if (!message.member.roles.cache.some(role => role.name === 'DJ')) return message.channel.send(
+            '> You need the `DJ` role to skip songs!\n To skip songs make a role named `DJ`!')
+
         let queue = await client.distube.getQueue(message);
         if (queue) {
             client.distube.stop(message)
