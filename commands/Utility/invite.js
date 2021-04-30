4f5840bc6e275023f7dcdfada65b7d453fa74e53
xavierk09 <xaviervavierk@gmail.com>
2021-04-30 08:11:13 +0530

    56#

---
+++ commands/Utility/invite.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'invite',
+    aliases: ['inv'],
+    execute(client, command, message, args, Discord){
+        message.channel.send(
+            new Discord.MessageEmbed()
+                .setColor('RANDOM')
+                .setTitle('Invite NearBeta from here!')
+                .setURL('https://discord.com/oauth2/authorize?client_id=822424076491554827&scope=bot&permissions=8')
+                .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
+             
+        )
+    }
+}
