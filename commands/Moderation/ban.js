9e3b0e91d57172a2c5644a3a245c512e4331e998
xavierk09 <xaviervavierk@gmail.com>
2021-05-02 13:46:25 +0530

    64#

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -3,7 +3,11 @@ module.exports = {
     category: "moderation",
     description: "bans a user",
     execute(client, command, message, args, Discord) {
-        //_________________________________________EMBEDS___________________________________________________________
+        let toBan = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args(0));
+
+        if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(perm1Embed)
+        if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send(perm2Embed)
+        
         const perm1Embed = new Discord.MessageEmbed()
             .setColor('RANDOM')
             .setTitle('Error!')
@@ -16,21 +20,15 @@ module.exports = {
             .setDescription('> I need the `BAN_MEMBERS` permission!')
             .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
 
+
+        const reason = args[1] || "There was no reason!";
+
         const bannedEmbed = new Discord.MessageEmbed()
             .setColor('RANDOM')
             .setTitle('Successfully banned!')
             .setDescription(`> ${toBan} has been banned from the server!\n**Reason**: ${reason}`)
             .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
-
-        //__________________________________________________________________________________________________________
-
-        let toBan = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args(0));
-
-        if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(perm1Embed)
-        if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send(perm2Embed)
-
-        const reason = args[1] || "There was no reason!";
-
+        
         toBan.ban({
             reason: reason
         })
