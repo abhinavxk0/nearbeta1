2048ad0dc107ca54df58d37fdd1a74c7e683ee56
xavierk09 <xaviervavierk@gmail.com>
2021-05-09 23:42:31 +0530

    84#

--- commands/Test/lyrics.js
+++ commands/Test/lyrics.js
@@ -1,16 +1,21 @@
-const Genius = require("genius-lyrics");
-const Client = new Genius.Client(""); 
+const solenolyrics= require("solenolyrics");
 
 module.exports = {
     name: 'lyrics',
     async execute(client, command, message, args, Discord){
-        const searches = await Client.songs.search(args.join(''));
-        const firstSong = searches[0];
-        const lyrics = await firstSong.lyrics();
 
-        try {message.channel.send("**Lyrics of the Song:**\n" + lyrics);
-        } catch (err){
-            console.log(err)
-        }
+    const lyrics = await solenolyrics.requestLyricsFor(args.join('')); 
+    const author = await solenolyrics.requestAuthorFor(args.join('')); 
+    const title = await solenolyrics.requestTitleFor(args.join('')); 
+    const pic = await solenolyrics.requestIconFor(args.join('')); 
+    
+    message.channel.send(
+        new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle(title + ' - ' + author)
+        .setThumbnail(pic)
+        .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
+        .setDescription(lyrics)
+    );
     }
 }
