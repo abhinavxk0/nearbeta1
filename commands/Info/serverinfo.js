479ff82554800407f7e0658816c5e0c030a09bd4
xavierk09 <xaviervavierk@gmail.com>
2021-05-09 09:12:06 +0530

    82#

---
+++ commands/Info/serverinfo.js
@@ -0,0 +1,33 @@
+module.exports = {
+    name: 'serverinfo',
+    async execute(client, command, message, args, Discord){
+        const { guild } = message
+
+        const { name, region, memberCount, owner, afkTimeout } = guild
+        const icon = guild.iconURL()
+
+    let embed = new Discord.MessageEmbed()
+      .setTitle(`Server info for "${name}"`)
+      .setThumbnail(icon)
+      .addFields(
+        {
+          name: 'Region',
+          value: region,
+        },
+        {
+          name: 'Members',
+          value: memberCount,
+        },
+        {
+          name: 'Owner',
+          value: owner.user.tag,
+        },
+        {
+          name: 'AFK Timeout',
+          value: afkTimeout / 60,
+        }
+      )
+
+    message.channel.send(embed)
+    }
+}
--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -4,8 +4,19 @@ module.exports = {
     description: "bans a user",
     execute(client, command, message, args, Discord) {
         let toBan = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args(0));
-
+        
+        const perm1Embed = new Discord.MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle('Error!')
+            .setDescription('> You need the `BAN_MEMBERS` permission!')
+            .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
         if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(perm1Embed)
+
+        const perm2Embed = new Discord.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('Error!')
+        .setDescription('> I need the `BAN_MEMBERS` permission!')
+        .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
         if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send(perm2Embed)
 
         const perm4Embed = new Discord.MessageEmbed()
@@ -14,26 +25,13 @@ module.exports = {
             .setDescription('> The target is higher than you in the roles hierarchy!')
             .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
         if (!message.member.roles.highest.position < toBan.roles.highest.position) return message.reply(perm4Embed)
+
         const perm3Embed = new Discord.MessageEmbed()
             .setColor('RANDOM')
             .setTitle('Error!')
             .setDescription('> The target is not bannable!')
             .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
-
-        if (!toBan.bannable) return message.channel.send(perm3Embed)
-
-        
-        const perm1Embed = new Discord.MessageEmbed()
-            .setColor('RANDOM')
-            .setTitle('Error!')
-            .setDescription('> You need the `BAN_MEMBERS` permission!')
-            .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
-
-        const perm2Embed = new Discord.MessageEmbed()
-            .setColor('RANDOM')
-            .setTitle('Error!')
-            .setDescription('> I need the `BAN_MEMBERS` permission!')
-            .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
+         if (!toBan.bannable) return message.channel.send(perm3Embed)
 
 
 
