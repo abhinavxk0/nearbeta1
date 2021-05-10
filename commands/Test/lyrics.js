9bd2f7bb0e6c3457aab7b0e486d5646ef501071a
xavierk09 <xaviervavierk@gmail.com>
2021-05-10 08:08:00 +0530

    85#

--- commands/Test/lyrics.js
+++ commands/Test/lyrics.js
@@ -1,21 +1,21 @@
-const solenolyrics= require("solenolyrics");
+const lyricsFinder = require('lyrics-finder');
 
 module.exports = {
     name: 'lyrics',
-    async execute(client, command, message, args, Discord){
+    async execute(client, command, message, args, Discord, artist, title){
 
-    const lyrics = await solenolyrics.requestLyricsFor(args.join('')); 
-    const author = await solenolyrics.requestAuthorFor(args.join('')); 
-    const title = await solenolyrics.requestTitleFor(args.join('')); 
-    const pic = await solenolyrics.requestIconFor(args.join('')); 
+    // const lyrics = await solenolyrics.requestLyricsFor(args.join('')); 
+    // const author = await solenolyrics.requestAuthorFor(args.join('')); 
+    // const title = await solenolyrics.requestTitleFor(args.join('')); 
+    // const pic = await solenolyrics.requestIconFor(args.join('')); 
+
+    const lyrics = await lyricsFinder(args.join('')) || "Not Found!\n Usage - `-lyrics (artist), (title)`";
     
     message.channel.send(
         new Discord.MessageEmbed()
         .setColor('RANDOM')
-        .setTitle(title + ' - ' + author)
-        .setThumbnail(pic)
         .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
         .setDescription(lyrics)
     );
     }
-}
+}   
