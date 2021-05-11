4a7de8d613b1e9e514c889bce1d0ad0fd1a64dc7
xavierk09 <xaviervavierk@gmail.com>
2021-05-11 10:23:33 +0530

    89#

--- commands/Music/queue.js
+++ commands/Music/queue.js
@@ -22,7 +22,7 @@ module.exports = {
                 .setTitle('Queue')
                 .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')
                 .setDescription('> Current queue:\n' + queue.songs.map((song, id) =>
-                    `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\`` + `\n\n> Volume : ${queue.volume}\n> Currently Playing: ${song.name} - ${queue.formattedCurrentTime}`
+                    `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``
                 ).slice(0, 10).join("\n"))
 
         );
