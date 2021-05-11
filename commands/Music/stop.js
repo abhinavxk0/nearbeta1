8cb72b84dc05e6e0911a1dd4af1875e0a6c23207
xavierk09 <xaviervavierk@gmail.com>
2021-05-11 21:24:00 +0530

    94#

--- commands/Music/stop.js
+++ commands/Music/stop.js
@@ -15,7 +15,7 @@ module.exports = {
         )
         if (!message.member.roles.cache.some(role => role.name === 'DJ')) return message.channel.send(
             new Discord.MessageEmbed()
-            .setColor('RANDOM')
+            .setColor('#2f3136')
             .setTitle('Error!')
             .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg')    
             .setDescription('> You need the `DJ` role to skip songs!\n To skip songs make a role named `DJ`!')        
@@ -27,7 +27,7 @@ module.exports = {
 
             message.channel.send(
                 new Discord.MessageEmbed()
-                    .setColor('RANDOM')
+                    .setColor('#2f3136')
                     .setTitle('Stopped!')
                     .setAuthor('NearBot Beta', 'https://cdn.discordapp.com/attachments/530277667119824917/834815044381966457/nearbot.jpg') 
                     .setDescription('> Stopped music playback and disconnected from the voice channel!')
