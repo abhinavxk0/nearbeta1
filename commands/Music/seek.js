3cbb29bb4c7e18f27b61ac8808bc20fa81dc5fdc
xavierk09 <xaviervavierk@gmail.com>
2021-05-13 12:19:43 +0530

    96#

--- commands/Music/seek.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'seek',
-    execute(client, command, message, args, Discord){
-        client.distube.seek(message, Number(args[0]));
-        message.react(
-            '⏩'
-        )
-    }
-}
