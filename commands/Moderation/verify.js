6daa7d82f9957681d1bb343c62729748a22a89b0
xavierk09 <xaviervavierk@gmail.com>
2021-06-13 00:26:13 +0530

    108#

--- commands/Moderation/verify.js
+++ commands/Moderation/verify.js
@@ -3,19 +3,11 @@ module.exports = {
     async execute(client, command, message, args, Discord){
 
         if (message.guild.id !== '768453184464748634') return message.reply('This command can only be used in another server.');
-        
-        if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send(`You aren't an admin!`)
 
         if(!args.length) return message.channel.send('This command, verifies a user!')
 
-        let user;
-        if (message.mentions.members.first()) {
-            user = message.mentions.members.first();
-        } else if (args[0]) {
-            user = message.guild.members.cache.get(args[0]).user;
-        } else {
-            user = message.author;
-        }
+        let user = message.author
+
 
         let role = user.guild.roles.cache.find(role => role.name === "⭒₊˚・Members");
         if (role) user.guild.members.cache.get(user.id).roles.add(role);
@@ -32,7 +24,10 @@ module.exports = {
         let role4 = user.guild.roles.cache.find(role4 => role4.name === "──────Arcade Access──────");
         if (role4) user.guild.members.cache.get(user.id).roles.add(role4);
 
-        message.channel.send("user verified.")
+        message.channel.send(
+            new Discord.MessageEmbed()
+            .setDescription('You have been verified!')
+        )
         
     }
 }
