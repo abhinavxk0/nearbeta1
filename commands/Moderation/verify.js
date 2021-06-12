b4a301b907c7ee27f27df753b973f60950e4927c
xavierk09 <xaviervavierk@gmail.com>
2021-06-13 00:36:38 +0530

    110#

--- commands/Moderation/verify.js
+++ commands/Moderation/verify.js
@@ -4,23 +4,20 @@ module.exports = {
 
         if (message.guild.id !== '768453184464748634') return message.reply('This command can only be used in another server.');
 
-        let user = message.author
+        let role = message.member.guild.roles.cache.find(role => role.name === "⭒₊˚・Members");
+        if (role) message.guild.members.cache.get(message.author.id).roles.add(role);
 
+        let role1 = message.member.guild.roles.cache.find(role1 => role1.name === "──────Levels──────");
+        if (role1) message.guild.members.cache.get(message.author.id).roles.add(role1);
 
-        let role = user.guild.roles.cache.find(role => role.name === "⭒₊˚・Members");
-        if (role) user.guild.members.cache.get(user.id).roles.add(role);
+        let role2 = message.member.guild.roles.cache.find(role2 => role2.name === "──────Self Roles──────");
+        if (role2) message.guild.members.cache.get(message.author.id).roles.add(role2);
 
-        let role1 = user.guild.roles.cache.find(role1 => role1.name === "──────Levels──────");
-        if (role1) user.guild.members.cache.get(user.id).roles.add(role1);
+        let role3 = message.member.guild.roles.cache.find(role3 => role3.name === "──────Ping Roles──────");
+        if (role3) message.guild.members.cache.get(message.author.id).roles.add(role3);
 
-        let role2 = user.guild.roles.cache.find(role2 => role2.name === "──────Self Roles──────");
-        if (role2) user.guild.members.cache.get(user.id).roles.add(role2);
-
-        let role3 = user.guild.roles.cache.find(role3 => role3.name === "──────Ping Roles──────");
-        if (role3) user.guild.members.cache.get(user.id).roles.add(role3);
-
-        let role4 = user.guild.roles.cache.find(role4 => role4.name === "──────Arcade Access──────");
-        if (role4) user.guild.members.cache.get(user.id).roles.add(role4);
+        let role4 = message.member.guild.roles.cache.find(role4 => role4.name === "──────Arcade Access──────");
+        if (role4) message.guild.members.cache.get(message.author.id).roles.add(role4);
 
         message.channel.send(
             new Discord.MessageEmbed()
