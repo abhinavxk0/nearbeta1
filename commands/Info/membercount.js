b6599878159b2df84f5cb3010141483aceb6a8ca
xavierk09 <xaviervavierk@gmail.com>
2021-06-16 23:08:33 +0530

    120#

--- commands/Info/membercount.js
+++ commands/Info/membercount.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'membercount',
-    export(client, command, message, args, Discord){
+    execute(client, command, message, args, Discord){
         let myGuild = client.guilds.cache.get("703315963583528991");
         let memberCount = myGuild.members.cache.filter(member => !member.user.bot).size;
     message.channel.send('Members: ' + memberCount);
