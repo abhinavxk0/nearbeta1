ab8652ac689b585bed74468d191a5688748a1cf0
xavierk09 <xaviervavierk@gmail.com>
2021-07-10 12:40:10 +0530

    160

--- commands/Utility/help.js
+++ commands/Utility/help.js
@@ -8,16 +8,9 @@ module.exports = {
             .setColor('#defafe')
             .setTitle('Help Command')
             .setThumbnail(client.user.displayAvatarURL({ size: 4096, dynamic: true }))
-            .addField([
-                `**Info:** \`anime, avatar, color, corona, mcserver, membercount, role-info, serverinfo, spotify, userinfo, youtube\``,
-                `**Moderation:** \`ban, clear, drag, hackban, kick, nick, resetnick, unban\``,
-                `**Actions:** \`hug, kiss, pat\``,
-                `**Fun:**\`8ball, afk, crackrate, dankmeme, simprate, wholesomememe\``,
-                `**Utility:** \`embed, help, invite, ping, servers, steal\``
-            ])
             .setDescription(
-                `The prefix for ${client.user} is \`n!\`!\n
-                [Invite NearBeta](https://discord.com/oauth2/authorize?client_id=822424076491554827&scope=bot&permissions=8) | [Support Server](https://discord.gg/CHg3UDcEuJ)`
+                `The prefix for ${client.user} is \`n!\`!\nTo see all ${client.user.username}'s go to the **[website](https://nearbeta.gitbook.io/nearbeta/)**!\n
+                **[Invite NearBeta](https://discord.com/oauth2/authorize?client_id=822424076491554827&scope=bot&permissions=8) | [Support Server](https://discord.gg/CHg3UDcEuJ) | [Website](https://nearbeta.gitbook.io/nearbeta/)**`
             )
             .setFooter('Thank you for choosing NearBeta!')
     message.channel.send(helpEmbed)
