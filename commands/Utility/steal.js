87beab489a0764addc09322779f2e9fec37de02e
xavierk09 <xaviervavierk@gmail.com>
2021-07-12 08:25:58 +0530

    163

--- commands/Utility/steal.js
+++ commands/Utility/steal.js
@@ -4,18 +4,22 @@ module.exports = {
     name: 'steal',
     aliases: ['add_emoji'],
     async execute(client, command, message, args, Discord) {
+
+        const EMOJIname = args[0]
+        const EMOJIS = args.slice(1).join('')
+
         if (!args.length) return message.channel.send(
             new Discord.MessageEmbed()
                 .setDescription('Please specify some emojis!')
                 .setColor('#defafe')
         )
-        for (const rawEmoji of args) {
+        for (const rawEmoji of EMOJIS) {
             const parsedEmoji = Util.parseEmoji(rawEmoji);
 
             if (parsedEmoji.id) {
                 const extension = parsedEmoji.animated ? ".gif" : ".png"
                 const url = `https://cdn.discordapp.com/emojis/${parsedEmoji.id + extension}`
-                message.guild.emojis.create(url, parsedEmoji.name)
+                message.guild.emojis.create(url, EMOJIname)
                     .then((emoji) =>             message.channel.send(
                         new Discord.MessageEmbed()
                             .setAuthor('Successfully added emoji(s)!')
