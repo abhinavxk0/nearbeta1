7c4fe8725b6be0da86ed5f83a576f55854cb4ea5
xavierk09 <xaviervavierk@gmail.com>
2021-08-02 18:07:37 +0530

    185

--- commands/Development/nowplaying.js
+++ commands/Development/nowplaying.js
@@ -4,14 +4,13 @@ module.exports = {
     description: 'shows song queue',
     async execute(client, command, message, args, Discord) {
 
-        let queue = client.distube.getQueue(message);
+        let dis = client.distube.Song(info, user, youtube);
+        let queue = client.distube.Queue(message, song);
         message.channel.send(
             new Discord.MessageEmbed()
                 .setColor('#2f3136')
                 .setAuthor('Queue')
-                .setDescription(`Current Track\n${client.song.name} - ${client.distube.queue.currentTime}` + queue.songs.map((song, id) =>
-                    `**${id + 1}**. ${song.name} - \`${song.formattedDuration}\``
-                ).slice(0, 10).join("\n") )
+                .setDescription(`Current Track\n${dis.name} - ${queue.currentTime}`)
         );
     }
 }   
