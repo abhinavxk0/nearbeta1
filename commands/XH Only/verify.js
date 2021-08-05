8886c88648ea1838901e43c69febf338d62f2fc3
xavierk09 <xaviervavierk@gmail.com>
2021-08-05 12:47:58 +0530

    191

--- commands/XH Only/verify.js
+++ commands/XH Only/verify.js
@@ -11,16 +11,16 @@ module.exports = {
         let role = message.member.guild.roles.cache.find(role => role.name === "꒷꒦︰Members ⋆˚. ✦");
         if (role) message.guild.members.cache.get(message.author.id).roles.add(role);
 
-        let role1 = message.member.guild.roles.cache.find(role1 => role1.name === "──────Levels──────");
+        let role1 = message.member.guild.roles.cache.find(role1 => role1.id === "────────Levels────────");
         if (role1) message.guild.members.cache.get(message.author.id).roles.add(role1);
 
-        let role2 = message.member.guild.roles.cache.find(role2 => role2.name === "──────Self Roles──────");
+        let role2 = message.member.guild.roles.cache.find(role2 => role2.name === "────────Self Roles────────");
         if (role2) message.guild.members.cache.get(message.author.id).roles.add(role2);
 
-        let role3 = message.member.guild.roles.cache.find(role3 => role3.name === "──────Ping Roles──────");
+        let role3 = message.member.guild.roles.cache.find(role3 => role3.name === "────────Ping Roles────────");
         if (role3) message.guild.members.cache.get(message.author.id).roles.add(role3);
 
-        let role4 = message.member.guild.roles.cache.find(role4 => role4.name === "──────Arcade Access──────");
+        let role4 = message.member.guild.roles.cache.find(role4 => role4.name === "───────Arcade Access──────");
         if (role4) message.guild.members.cache.get(message.author.id).roles.add(role4);
 
         message.channel.send(
