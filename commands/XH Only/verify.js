b3681406188452ece7286d6a89efa40257ae46cc
xavierk09 <xaviervavierk@gmail.com>
2021-09-20 13:45:15 +0530

    198 Commit (Stable)

--- commands/XH Only/verify.js
+++ commands/XH Only/verify.js
@@ -8,19 +8,19 @@ module.exports = {
 
         if (message.guild.id !== '768453184464748634') return message.reply('This command can only be used in another server.');
 
-        let role = message.member.guild.roles.cache.find(role => role.name === "꒷꒦︰Members ⋆˚. ✦");
+        let role = message.member.guild.roles.cache.find(role => role.id === "768460407189798952");
         if (role) message.guild.members.cache.get(message.author.id).roles.add(role);
 
-        let role1 = message.member.guild.roles.cache.find(role1 => role1.id === "────────Levels────────");
+        let role1 = message.member.guild.roles.cache.find(role1 => role1.id === "806823435350573088");
         if (role1) message.guild.members.cache.get(message.author.id).roles.add(role1);
 
-        let role2 = message.member.guild.roles.cache.find(role2 => role2.name === "────────Self Roles────────");
+        let role2 = message.member.guild.roles.cache.find(role2 => role2.id === "807536591459450881");
         if (role2) message.guild.members.cache.get(message.author.id).roles.add(role2);
 
-        let role3 = message.member.guild.roles.cache.find(role3 => role3.name === "────────Ping Roles────────");
+        let role3 = message.member.guild.roles.cache.find(role3 => role3.id === "825285084830892038");
         if (role3) message.guild.members.cache.get(message.author.id).roles.add(role3);
 
-        let role4 = message.member.guild.roles.cache.find(role4 => role4.name === "───────Arcade Access──────");
+        let role4 = message.member.guild.roles.cache.find(role4 => role4.id === "822367042329509889");
         if (role4) message.guild.members.cache.get(message.author.id).roles.add(role4);
 
         message.channel.send(
