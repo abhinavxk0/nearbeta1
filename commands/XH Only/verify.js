eaa4ccc6c4dd1f95ce3b5ceca59738dab8da52a7
xavierk09 <xaviervavierk@gmail.com>
2021-10-14 11:34:57 +0530

    199 Commit (Stable)

--- commands/XH Only/verify.js
+++ commands/XH Only/verify.js
@@ -23,6 +23,14 @@ module.exports = {
         let role4 = message.member.guild.roles.cache.find(role4 => role4.id === "822367042329509889");
         if (role4) message.guild.members.cache.get(message.author.id).roles.add(role4);
 
+        let role5 = message.member.guild.roles.cache.find(role5 => role5.id === "884478480190943272");
+        if (role5) message.guild.members.cache.get(message.author.id).roles.add(role5);
+
+        let role6 = message.message.guild.roles.cache.find(role6 => role6.id === '892725421102206986');
+        if (role6) message.guild.members.cache.get(message.author.id).roles.add(role6);
+
+        
+
         message.channel.send(
             new Discord.MessageEmbed()
             .setDescription('You have been verified!')
