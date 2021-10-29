612e23f4a3104b6482b7b5f132abcd72bc165b14
xavierk09 <xaviervavierk@gmail.com>
2021-10-29 12:21:06 +0530

    pm2 remove

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'restart',
+    name: 'forcestop',
     async execute(client, command, message, args, Discord){
 
         if (message.author.id !== '307777831625293825') return;
