b9fd389c5a9d14bb3e29249b253c518511f35edf
xavierk09 <xaviervavierk@gmail.com>
2021-11-04 04:34:43 +0530

    report command added v3

--- commands/Utility/report.js
+++ commands/Utility/report.js
@@ -4,10 +4,11 @@ module.exports = {
     async execute(client, command, message, args, Discord) {
         const report = args.join('');
         const xavier = await client.users.cache.get('307777831625293825')
+        const user = message.mentions.users.first() || message.author
 
         const repembed = new Discord.MessageEmbed()
             .setColor('#defafe')
-            .setAuthor(message.author.tag, message.author.displayAvatar({ size: 4098, dynamic: true }))
+            .setAuthor(message.author.tag, user.displayAvatar({ size: 4098, dynamic: true }))
             .setDescription(report)
 
         xavier.send(repembed)
